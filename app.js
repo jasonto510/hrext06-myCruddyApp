09cc14762c525f10bb16f7d3ca6819397114e0cf
jasonto <jasto@ucdavis.edu>
2018-12-18 16:29:11 -0800

    stored multiple values in an array

--- app.js
+++ app.js
@@ -1,21 +1,50 @@
 
+var array = [];
+
 $(document).ready(function(){
   console.log('jQuery loaded');
 
   // write to local storage from input when button save clicked
   $('.btn-submit').on('click', function(){
 
-//setItem is the method used for adding some data, takes in your key and value
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
-    var myItemInStorage = localStorage.getItem('inputFieldValue');
-    console.log('myItemInStorage', myItemInStorage);
-    localStorage.setItem("Hi", "You");
-    var artist = localStorage.getItem("Hi")
-    console.log(artist)
+    // var myItemInStorage = localStorage.getItem('inputFieldValue');
+    // console.log('myItemInStorage', myItemInStorage);
+
+    var user = localStorage.getItem('inputFieldValue');
+    user = user ? user.split(',') : []
+    var userText =  $('.text-entry').val()    
+    user.push(userText);
+    localStorage.setItem('inputFieldValue', user.toString())
+
+
+
+
+    // localStorage.setItem('inputFieldValue', $('.text-entry').val());
+    // var myItemInStorage = localStorage.getItem('inputFieldValue');
+    // console.log('myItemInStorage', myItemInStorage);
+
+
+    //Have an array
+      //If the array length is > 0 then push the numbers into the array
+
+
+
+
+
+
+
+
+
 
     // localStorage.setItem("inputFieldValue",  $('.text-entry').val())
     // var data = localStorage.getItem("inputFieldValue")
-    
+    // var input = $('.text-entry').val();
+
+
+    // function store($('.text-entry').val(){
+    //   array.push($('.text-entry').val());
+    //   localStorage.setItem("inputFieldValue", JSON.stringify(array))
+    // }
     // display the value here
     $('.list-display-field').text(myItemInStorage); // ??
 
@@ -24,7 +53,7 @@ $(document).ready(function(){
 
   // delete from local storage when delete button clicked
   $('.btn-delete').on('click', function(){
-    localStorage.removeItem('inputFieldValue');
+    localStorage.removeItem('inputFieldValue'); //Only removes the last item in local storage with the given key
   });
 
   //Delete all items from local storage when clicked 
